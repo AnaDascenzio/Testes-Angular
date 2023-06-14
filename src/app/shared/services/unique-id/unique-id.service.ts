b5cc693725dde96bd76b98da171eb67dae0347e9
Ana Nicolly BF Dascenzio <ananicolly.dascenzio@shift.com.br>
2023-06-14 10:17:50 -0300

    condição no  metodo de gerar com prefixo

--- src/app/shared/services/unique-id/unique-id.service.ts
+++ src/app/shared/services/unique-id/unique-id.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import {v4 as uuidv4} from 'uuid';
+import {stringify, v4 as uuidv4} from 'uuid';
 
 @Injectable()
 export class UniqueIdService{
@@ -7,6 +7,9 @@ export class UniqueIdService{
   private numberOfGeneratedIds = 0;
 
   public generateUniqueIdWithPrefix(prefix: string): string{
+    if(!prefix){
+    throw Error('Prefix can not be empty');
+    }
     const uniqueId = this.generateUniqueId();
     this.numberOfGeneratedIds++;
     return `${prefix}-${uniqueId}`;
