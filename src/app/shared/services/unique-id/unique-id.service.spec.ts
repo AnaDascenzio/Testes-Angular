051e9ec690d628b5464a1938c864137381c189f1
Ana Nicolly BF Dascenzio <ananicolly.dascenzio@shift.com.br>
2023-06-15 09:11:17 -0300

    Lógica de testes

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -1,10 +1,30 @@
-import { UniqueIdService } from "./unique-id.service";
+import { UniqueIdService } from "./unique-id.service"
+
 
 describe(UniqueIdService.name, ()=>{
+
+    let service: UniqueIdService;
+
+    beforeEach(()=>{
+        service = new UniqueIdService();
+    })
+
     it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate id when called with prefix`, ()=>{
-        const service = new UniqueIdService();
         const id = service.generateUniqueIdWithPrefix('app');
-        expect(id).toContain('app-');
+        expect(id.startsWith('app-')).toBeTrue();
     });
+
+    it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should not generate duplicate ID when called multiple times`, ()=>{
+        const ids = new Set();
+        for (let i = 0; i < 50; i++){
+            ids.add(service.generateUniqueIdWithPrefix('app'));
+        }
+        expect(ids.size).toBe(50);
+    })
+    it(`#${UniqueIdService.prototype.getNumberOfGeneratedUniqueIds.name} should return the number of generatedIds when called`, ()=>{
+       service.generateUniqueIdWithPrefix('app');
+       service.generateUniqueIdWithPrefix('app');
+       expect(service.getNumberOfGeneratedUniqueIds()).toBe(2);
+       })
 });
 
